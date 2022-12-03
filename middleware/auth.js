c66e2a9807d446de47dbc563cb724a54c78e075b
emirulmukmin <82020237+emirulmukmin@users.noreply.github.com>
2022-12-03 11:43:57 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,14 @@ const jwt = require('jsonwebtoken');
 SECRET = process.env.SECRET
 const Auth = {
     verifyToken(req, res, next){ 
-        const token = req.cookies["jwt"];
-        //const {token} = req.body; localstorage sesi mingdep
+        //const token = req.cookies["jwt"];
+        const {token} = req.body; 
         if (token) {
             // 12. Lalukan jwt verify
             const verified = jwt.verify(token, SECRET)
             if(verified){
               console.log("You're authenticated");
-              //req.verified = verified utk modul storage
+              req.verified = verified;
               return next();
         } else {
           res.status(403).send({message: 'Youre not authenticated, please login first'})
