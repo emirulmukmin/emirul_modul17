ee4b6288924d9a2fc20b63bd0dee453055393a64
emirulmukmin <82020237+emirulmukmin@users.noreply.github.com>
2022-12-03 11:43:16 +0700

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -65,8 +65,8 @@ const logout = async(req, res, next) => {
 const verify = async(req, res, next) => {
     try {
         // 13. membuat verify
-        //const email = req.verified.email; utk modul storage
-        const {email} = req.body
+        const email = req.verified.email;
+        //const {email} = req.body
         const foundUser = await db.query(`SELECT * FROM unhan_modul17 WHERE email=$1 LIMIT 1`, [email])
         return res.status(200).json({
             id       : foundUser.rows[0].id,
